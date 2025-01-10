a52c4a4c23c5f3fc5e90646a40de8e1653aa4ccc
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-01-10 16:36:24 +0300

    update

---
+++ chaining.js
@@ -0,0 +1,8 @@
+// method chaining = calling one method after another
+//                   in one continuous line of code
+
+let userName = "timothy";
+
+let letter = userName.charAt(0).toUpperCase().trim();
+
+console.log(letter);
---
+++ string.js
@@ -0,0 +1,16 @@
+// useful string properties & methods
+
+let userName = "Timothy Manya";
+let phoneNumber = "+2547-959-78606";
+
+//console.log(userName.length);
+//console.log(userName.charAt(0));
+//console.log(userName.indexOf("o"));
+//console.log(userName.lastIndexOf("o"));
+//userName = userName.trim();
+//userName = userName.toUpperCase();
+//userName = userName.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-", "");
+
+console.log(phoneNumber);
