e73b376e3aabee2a3d88e7f75b9045550927ea9f
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-02-05 17:14:32 +0300

    update

---
+++ arrow function.js
@@ -0,0 +1,6 @@
+const greeting =username =>console.log('Hello ${username}');
+greetings(timothy);
+
+let prices = [5, 10, 15, 20, 25];
+grades.sort((x ,y) =>y-x);
+grades.forEach((element) =>console.log(element));
