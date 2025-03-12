6e318a40833ae0b8ba082062d1e0af9e3b1a7d99
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-03-12 18:05:16 +0300

    update

---
+++ asynchronous functions/async.js
@@ -0,0 +1,19 @@
+//synchronous functions
+console.log ("i ");
+console.log ("love  ");
+console.log (" coding");
+console.log ("daily ");
+console.log ("morning ");
+
+
+//asynchronous function
+console.log ("i ");
+console.log ("love  ");
+
+setTimeout(()=>{
+    console.log (" coding");
+    
+} ,5000); //it displays after 5000ms
+
+console.log ("daily ");
+console.log ("morning ");
